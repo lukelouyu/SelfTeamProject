ea631af612bea9beaa0aad544e6b5f703453d26b
Luke Louyu <86088194+lukelouyu@users.noreply.github.com>
2026-07-30 21:36:38 +0800

    Add ActivityOrder: INPUT/TIME/CHRONOLOGICAL enum
    Used by the next step to add sorting to ActivityManager.list(), and
    later by order view/order set once ActivityCommandParser exists.

---
+++ src/main/java/seedu/unienable/model/enums/ActivityOrder.java
@@ -0,0 +1,8 @@
+package seedu.unienable.model.enums;
+
+/** How activities are ordered by list/find when no one-shot override is supplied. */
+public enum ActivityOrder {
+    INPUT,
+    TIME,
+    CHRONOLOGICAL
+}
